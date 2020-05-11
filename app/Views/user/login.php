29db4d46733efaf5374b0586301c67094d6eae14
Brenda Santos <brenda2santos@outlook.com>
2020-05-11 15:46:58 -0300

    login screen - base

---
+++ app/Views/user/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor Guincho - LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?> /assets/css/login.css" >
+</head>
+
+<body>
+    <div class='div_form'>
+
+    <img src="" alt="">
+        <h1>LOGIN</h1>
+        <form method='POST' action='process_login.php'>
+            <input type="login" placeholder='Usuário'>    
+            <input type="password" placeholder='Senha'>
+            <input id="button_login" type="submit" value='Entrar'>
+        </form>
+
+    </div>
+
+    
+</body>
+
+</html>
